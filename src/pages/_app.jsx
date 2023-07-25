5b3758fae050fe73ed7996bc6059b1c3a9f39b82
= <=>
2023-07-25 18:51:04 +0530

    fix: stuck site when connect to the wallet

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -15,7 +15,6 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../styles/style.scss";
 import "@rainbow-me/rainbowkit/styles.css";
-import React, { useEffect } from "react";
 
 const appProvider =
   typeof window !== "undefined" && window?.ethereum
@@ -48,17 +47,6 @@ function StakingApp({ Component, pageProps }) {
     LOTTERYABI,
     Signer
   );
-
-  useEffect(() => {
-    if (window.ethereum) {
-      window.ethereum.on('chainChanged', () => {
-        window.location.reload();
-      })
-      window.ethereum.on('accountsChanged', () => {
-        window.location.reload();
-      })
-    }
-  }, [])
 
   return (
     <motion.section
